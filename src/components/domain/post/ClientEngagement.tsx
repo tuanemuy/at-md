70b0ff3f4d22ea2adc021c3f19f9eb02581794c6
tuanemuy <22880537+tuanemuy@users.noreply.github.com>
2025-04-25 06:36:11 +0900

    debug

--- src/components/domain/post/ClientEngagement.tsx
+++ src/components/domain/post/ClientEngagement.tsx
@@ -3,38 +3,45 @@
 import { getEngagementByNotePath } from "@/actions/post";
 import type { Note } from "@/domain/note/models/note";
 import type { Engagement as EngagementModel } from "@/domain/post/models/engagement";
+import { useTransition } from "react";
 import { useEffect, useState } from "react";
 
 import { Heart, MessageCircle } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {
   note: Note;
 };
 
 export function Engagement({ note }: Props) {
+  const [isPending, startTransition] = useTransition();
   const [engagement, setEngagement] = useState<EngagementModel | null>(null);
   useEffect(() => {
-    (async () => {
+    startTransition(async () => {
       const engagement = await getEngagementByNotePath(note.bookId, note.path);
       setEngagement(engagement);
-    })();
+    });
   }, [note.bookId, note.path]);
 
-  if (!engagement) {
-    return null;
+  if (engagement) {
+    return (
+      <div className="flex items-center gap-3 text-sm text-muted-foreground">
+        <div className="flex items-center gap-1">
+          <Heart size={20} />
+          {engagement.likes}
+        </div>
+
+        <div className="flex items-center gap-1">
+          <MessageCircle size={20} />
+          {engagement.replies}
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div className="flex items-center gap-3 text-sm text-muted-foreground">
-      <div className="flex items-center gap-1">
-        <Heart size={20} />
-        {engagement.likes}
-      </div>
+  if (!engagement && isPending) {
+    return <Skeleton className="h-8 w-24" />;
+  }
 
-      <div className="flex items-center gap-1">
-        <MessageCircle size={20} />
-        {engagement.replies}
-      </div>
-    </div>
-  );
+  return null;
 }
